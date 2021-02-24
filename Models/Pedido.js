57f5c8912daf8487752c7947556705c8866902dc
Elias Alejandro <eaeliasalejandro@hotmail.com>
2021-02-23 22:04:41 -0600

    clases js

---
+++ Models/Pedido.js
@@ -0,0 +1,30 @@
+class Pedido {
+    constructor(id, id_cliente, id_productos, costo, estatus) {
+        this.id = id;
+        this.id_cliente = id_cliente;
+        this.id_productos = id_productos;
+        this.costo = costo;
+        this.estatus = estatus;
+    }
+    crear() {
+
+    }
+    cancelar() {
+
+    }
+    eliminar() {
+
+    }
+    editar() {
+
+    }
+    verPedido() {
+
+    }
+    verHistorial() {
+
+    }
+    filtrar() {
+
+    }
+}
---
+++ Models/Producto.js
@@ -0,0 +1,29 @@
+class Producto {
+    constructor(id, nombre, id_categoria, descripcion, estatus_disponibilidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.id_categoria = id_categoria;
+        this.descripcion = descripcion;
+        this.costo = costo;
+        this.estatus_disponibilidad = estatus_disponibilidad;
+    }
+    crear() {
+
+    }
+    desactivar() {
+
+    }
+    activar() {
+
+    }
+    editar() {
+
+    }
+
+    ver() {
+
+    }
+    filtrar() {
+
+    }
+}

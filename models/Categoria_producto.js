28f2f61256f52fd0ca55541516c1afa83aad9f34
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-02-25 18:59:35 -0700

    [Postwork 04] - Copia de modelos del postwork03

---
+++ models/Categoria_producto.js
@@ -0,0 +1,26 @@
+class Categoria_producto {
+    constructor(id, nombre, descripcion, estatus) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estatus = estatus;
+    }
+    crear() {
+
+    }
+    desactivar() {
+
+    }
+    activar() {
+
+    }
+    editar() {
+
+    }
+    ver() {
+
+    }
+    filtrar() {
+
+    }
+}
---
+++ models/Usuario.js
@@ -0,0 +1,37 @@
+class Usuario {
+    constructor(id, nombre, correo, contraseña, tipo, estatus) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.tipo = tipo;
+        this.estatus = estatus;
+    }
+    iniciarSesion() {
+
+    }
+    cerrarSesion() {
+
+    }
+    registrarse() {
+
+    }
+    ver() {
+
+    }
+    filtrar() {
+
+    }
+    editar() {
+
+    }
+    cambiarRol() {
+
+    }
+    activar() {
+
+    }
+    desactivar() {
+
+    }
+}

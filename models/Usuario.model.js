3e4ce9cd4bbeb63cc5d5f6f095165f12bbeb681c
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-03-11 17:30:42 -0700

    [Final Project] User Model

--- models/Usuario.model.js
+++ models/Usuario.model.js
@@ -1,13 +1,74 @@
-//Clase de usuario para poder instanciar nuevos objetos de este tipo
-class Usuario {
-    constructor({id, nombre, correo, password, tipo, estatus}) {
-        this.id = id;
-        this.nombre = nombre;
-        this.correo = correo;
-        this.password = password;
-        this.tipo = tipo;
-        this.estatus = estatus;
-    }
-}
+const mongoose = require('mongoose');
+const uniqueValidator = require("mongoose-unique-validator");
+const UsuarioSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: {
+        type: String,
+        unique: true, //este campo no se puede repetir
+        lowercase: true,
+        required: [true, "no puede estar vacío"],
+        match: [/\S+@\S+\.\S+/, "es inválido"],
+        index: true,
+    },
+    type: { type: String, enum: ['cliente', 'mesero', 'chef', 'admin'] },
+    status: { type: Number },
+    hash: String,
+    salt: String
+},
+    { timestamps: true }
+);
 
-module.exports = Usuario;
+// usando plugin de validación para que no se repitan correos
+UsuarioSchema.plugin(uniqueValidator, { message: "Ya existe" });
+
+UsuarioSchema.methods.hashPassword = function (password) {
+    this.salt = crypto.randomBytes(16).toString("hex"); // generando una "sal" random para cada usuario
+    this.hash = crypto
+        .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+        .toString("hex"); // generando un hash utilizando la sal
+};
+
+UsuarioSchema.methods.validatePassword = function (password) {
+    const hash = crypto
+        .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+        .toString("hex");
+    return this.hash === hash;
+};
+
+UsuarioSchema.methods.generarJWT = function () {
+    const today = new Date();
+    const exp = new Date(today);
+    exp.setDate(today.getDate() + 60); // 60 días antes de expirar
+
+    return jwt.sign({
+        id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000),
+    }, secret);
+};
+
+UsuarioSchema.methods.toAuthJSON = function () {
+    return {
+        username: this.username,
+        email: this.email,
+        type: this.type,
+        status: this.status,
+        token: this.generarJWT()
+    };
+};
+
+UsuarioSchema.methods.publicData = function () {
+    return {
+        id: this.id,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        type: this.type,
+        status: this.status,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt
+    };
+};
+
+mongoose.model("Usuario", UsuarioSchema);

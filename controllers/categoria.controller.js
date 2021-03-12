398e4f83045773887a32ee258cf9eaff2bf76f03
Nathaly N Dimas C <nathaly.ndc@hotmail.com>
2021-03-11 23:07:23 -0600

    categoria-v1

--- controllers/categoria.controller.js
+++ controllers/categoria.controller.js
@@ -1,88 +1,87 @@
-// importamos el modelo de categoria
-const { Categoria } = require('../models')
-//creamos arreglo de objetos para los datos
-const CATEGORIAS = [
-  new Categoria({ id: 1, nombre: 'Desayunos', descripcion: 'Es la cat de desayunos servida de 7-9am', estatus: 1 }),
-  new Categoria({ id: 2, nombre: 'Jugos', descripcion: 'Es la cat de jugos naturales', estatus: 1 }),
-  new Categoria({ id: 3, nombre: 'Sodas', descripcion: 'Es la cat de refrescos', estatus: 1 })
-]
 
-const crearCategoria = (req, res) => {
+// controllers/categoria.js
+const mongoose = require("mongoose")
+const Categoria = mongoose.model("Categoria")
+
+//--------------------------------------------------------------------
+const crearCategoria = (req, res,next) => {
+  if(req.usuario.type!=="admin"){
+    return res.status(401).send("sin permisos")
+  }
   // Instanciaremos una nueva categoria utilizando la clase categoria
   let categoria = new Categoria(req.body)
-
-  if (!!categoria.id) { //Validar si la nueva categoria tiene id
-    CATEGORIAS.push(categoria) //agregar al arreglo
-    res.status(201).send(categoria) //enviarla como respuesta al ser exitosa la llamada
-  }
-  else {
-    res.status(304).send({ Message: 'Not Modified: No se agregó producto vacío' }); //ya que no se pueden agregar vacios se envia al siguiente mensaje
-  }
+    categoria.save().then(categ => {    //Guardando nuevo usuario en MongoDB.
+      return res.status(201).json(categ)
+    }).catch(next)
 }
 
-const verCategorias = (req, res) => {
-  res.status(200).send(CATEGORIAS) //envia todos los datos de categorias
-}
-
-const verCategoria = (req, res) => {
-  let { id } = req.params;
-  let categoria = CATEGORIAS.filter((categoria) => categoria.id === +id); //Filtra la categoría con cierto id
-  if (!!categoria[0]) {
-    res.status(200).send(categoria[0]);
-  } else {
-    res.status(400).send({ errorMessage: 'Not Found: No existe el usuario' });
+function verCategoria(req, res, next) {                              //Obteniendo categoria desde MongoDB.
+  if(req.usuario.type==="chef"){
+    return res.status(401).send("sin permisos")
   }
-};
-
-
-const editarCategoria = (req, res) => {
-  // toda la informacion necesaria va en req.body
-  let datos = req.body;
-  let categoriaEdited = null; //variable para guardar la categoria editada
-  for (let i = 0; i < CATEGORIAS.length; i++) {
-    if (CATEGORIAS[i].id === datos.id) { //cuando encuentre la categoria acorde a su id
-      for (campo in datos) {
-        CATEGORIAS[i][campo] = datos[campo]; //cambia el valor de la propiedad del objeto indicado por el nuevo
-        categoriaEdited = CATEGORIAS[i]; //guarda este objeto modificado para ser mostrado en la respuesta
-      }
+  Categoria.findById(req.params.id, (err, categ) => {
+    if (!categ || err) {
+      return res.sendStatus(401)
     }
-  }
-  if (categoriaEdited) {
-    res.status(200).send(categoriaEdited);
-  } else {
-    res.status(404).send({ errorMessage: 'Not Found: Categoría no encontrada' });
-  }
+    return res.json(categ.publicData());
+  }).catch(next);
 }
 
-const cambiarEstatusCategoria = (req, res) => {
-  let categoriaEdited = null; //variable para guardar la categoria editada
-  for (let i = 0; i < CATEGORIAS.length; i++) {
-    if (CATEGORIAS[i].id === req.body.id) { //cuando encuentre la categoria acorde a su id
-      CATEGORIAS[i].estatus = req.body.estatus; //se cambia el estatus de la seleccion acorde al estatus nuevo que se le envia
-      categoriaEdited = CATEGORIAS[i]; //guarda el objeto
+
+function verCategorias(req, res, next) {   
+  if(req.usuario.type==="chef"){
+    return res.status(401).send("sin permisos")
+  }                           //Obteniendo categorias desde MongoDB.
+  Categoria.find((err, categ) => {
+    if (!categ || err) {
+      return res.sendStatus(401)
     }
-  }
-  if (categoriaEdited) {
-    res.status(200).send(categoriaEdited);
-  }
-  else {
-    res.status(404).send({ errorMessage: 'Not found' });
-  }
+    return res.json(categ);
+  }).catch(next);
 }
 
-const filtrarCategoria = (req, res) => {
+function editarCategoria(req, res, next) {
+  if(req.usuario.type!=="admin"){
+    return res.status(401).send("sin permisos")
+  }
+  Categoria.updateOne({_id:req.params.id},{$set:req.body}).then(categ => {//pendiente de revisar/arreglar
+    if (!categ) { return res.sendStatus(401); }
+    res.status(201).json(categ)
+  }).catch(next)
+}
+
+function cambiarEstatusCategoria(req, res, next) {//pendiente de probar
+  if(req.usuario.type!=="admin"){
+    return res.status(401).send("sin permisos")
+  }
+  console.log(req.categoria)
+  Categoria.findById(req.categoria.id).then(categ => {
+    if (!categ) { return res.sendStatus(401); }
+    let nuevaInfo = req.body
+    if (typeof nuevaInfo.status !== 'undefined')
+      categ.status = nuevaInfo.status
+    categ.save().then(updatedCateg => {                                   //Guardando usuario modificado en MongoDB.
+      res.status(201).json(updatedCateg.publicData())
+    }).catch(next)
+  }).catch(next)
+}
+
+function filtrarCategoria(req, res, next) { 
+  if(req.usuario.type==="chef"){
+    return res.status(401).send("sin permisos")
+  } 
   let campo = Object.keys(req.body)[0]; //toma el nombre de la propiedad del objeto por la que se va a filtrar
-  let dato = req.body[campo]; //guarda el valor de esta propiedad (valor a buscar)
-  let categoriasFiltradas = CATEGORIAS.filter(categoria => {
-    let regex = new RegExp(dato, 'gi'); //utiliza expresion regular para hacer busqueda global e ignorar capitalizacion de letras
-    return regex.test(categoria[campo]);
-  });
-
-  if (!!categoriasFiltradas[0]) {
-    res.status(200).send(categoriasFiltradas); //muestra resultados encontrados
-  } else {
-    res.status(404).send({ errorMessage: 'NotFound: Busqueda no arrojó resultados' });
-  }
+  let dato = req.body[campo]; //guarda el valor de esta propiedad (valor a buscar)                             //Obteniendo categoria desde MongoDB.
+  let filter = {}
+  filter[campo]= new RegExp(`${dato}`, "i");
+  
+  Categoria.find(filter).then ((categ,err) => {
+       console.log(err);
+    if (!categ || err) { 
+      return res.sendStatus(401)
+    }
+    return res.json(categ);
+  }).catch(next);
 }
 
 // exportamos las funciones definidas
--- models/Categoria.model.js
+++ models/Categoria.model.js
@@ -1,11 +1,33 @@
-//Clase de categoria para poder instanciar nuevos objetos de este tipo
-class Categoria {
-    constructor({id, nombre, descripcion, estatus}) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.estatus = estatus;
-    }
-}
-//Exportar modelo
-module.exports = Categoria;
+const mongoose = require('mongoose');  // exportacion de mongoose
+const uniqueValidator = require("mongoose-unique-validator"); //Importando módulo mongoose-unique-validator, pendiente de instalar.
+
+//Definiendo cada campo con sus tipo sde datos y las validaciones sobre este.
+const CategoriaSchema = new mongoose.Schema({                   
+name: {                                                  
+  type: String,
+  unique: true, //este campo no se puede repetir
+  required: [true, "no puede estar vacío"],
+  index: true,
+},                                           
+description: { type: String, required: [true, "no puede estar vacío"] },
+status: {type: Number, required: [true, "no puede estar vacío"] }
+},
+{ timestamps: true }
+);
+
+
+CategoriaSchema.methods.publicData = function () {
+    return {
+        id: this.id,
+        name: this.name,
+        description: this.description,
+        status: this.status,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt
+    };
+};
+
+
+// usando plugin de validación para que no se repitan correos ni usernames
+CategoriaSchema.plugin(uniqueValidator, { message: "Ya existe" }); 
+mongoose.model("Categoria", CategoriaSchema);    //Define el modelo Usuario, utilizando el esquema UsuarioSchema.

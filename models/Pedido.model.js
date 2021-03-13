69ccf4861eac4c0b22071316d50f109c966109dd
Adrian <adrian-bt@hotmail.com>
2021-03-12 23:11:38 -0600

    pedido controller update

--- models/Pedido.model.js
+++ models/Pedido.model.js
@@ -1,19 +1,22 @@
-const mongoose = require('mongoose');  // exportacion de mongoose
+const mongoose = require("mongoose"); // exportacion de mongoose
 
 //Definiendo cada campo con sus tipo sde datos y las validaciones sobre este.
 const PedidoSchema = new mongoose.Schema(
-    {
-        client: { type: mongoose.ObjectId, required: [true, "no puede estar vacío"], index: true },
-        chef:{type:mongoose.ObjectId, default:null},
-        mesero:{type:mongoose.ObjectId, default:null},
-        info: { type: Array, required: [true, "no puede estar vacío"] },
-        cost: { type: Number, required: [true, "no puede estar vacío"] },
-        status: { type: Number, required: [true, "no puede estar vacío"] }
+  {
+    client: {
+      type: mongoose.ObjectId,
+      required: [true, "no puede estar vacío"],
+      index: true,
     },
-    {
-        timestamps: true
-    }
+    chef: { type: mongoose.ObjectId, default: null },
+    mesero: { type: mongoose.ObjectId, default: null },
+    info: { type: Array, required: [true, "no puede estar vacío"] },
+    cost: { type: Number, required: [true, "no puede estar vacío"] },
+    status: { type: Number, required: [true, "no puede estar vacío"] },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-
-mongoose.model("Pedido", PedidoSchema);    //Define el modelo Usuario, utilizando el esquema UsuarioSchema.
+mongoose.model("Pedido", PedidoSchema); //Define el modelo Usuario, utilizando el esquema UsuarioSchema.

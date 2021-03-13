3271b3387795a881f7c963140f6f7f29a45157da
Adrian <adrian-bt@hotmail.com>
2021-03-12 23:11:18 -0600

    Co-authored-by: José Antonio Rodríguez Hernández <shepe_rodriguez@hotmail.com> Co-authored-by: nathaly.ndc@hotmail.com <nathaly.ndc@hotmail.com>

--- controllers/pedido.controller.js
+++ controllers/pedido.controller.js
@@ -1,154 +1,162 @@
-const mongoose = require('mongoose');  // exportacion de mongoose
-const Pedido = mongoose.model('Pedido');
+const mongoose = require("mongoose"); // exportacion de mongoose
+const Pedido = mongoose.model("Pedido");
+const Producto = mongoose.model("Producto");
 
 const crearPedido = (req, res, next) => {
-  if(req.usuario.type!=="admin") return res.status(401).send("sin permisos")
-  
-  let pedido = new Pedido(req.body)
-    pedido.save().then(pedido => {    //Guardando nuevo usuario en MongoDB.
-      return res.status(201).json(pedido)
-    }).catch(next);
-}
+  if (req.usuario.type !== "admin") return res.status(401).send("sin permisos");
 
-/* Verbo   Ruta            Body        Respuesta 
-POST    skdfljhskdlfh   {sdfadfa}   
-PUT      */
+  let pedido = new Pedido(req.body);
+  pedido
+    .save()
+    .then((pedido) => {
+      //Guardando nuevo usuario en MongoDB.
+      return res.status(201).json(pedido);
+    })
+    .catch(next);
+};
 
-//Pedido 604c33f09de26c34e254b386
-
-const verPedido = (req, res, next) => { //envia los datos del pedido seleccionado
-  Pedido.findById(req.params.id).populate('producto', 'nombre id_categoria descripcion costo').then((pedido,err) => {
-    console.log('verPEd');
-    if (!pedido || err) {
-      return res.sendStatus(401)
-    }
-    return res.json(pedido);
-  }).catch(next);
-}
-
-/* 
-604ae551102f226520d85cb0
-604ae5dc102f226520d85cb2
-604ae5e5102f226520d85cb3
-604ae7e0261f1367a35f216c
- */
+const verPedido = (req, res, next) => {
+  //envia los datos del pedido seleccionado
+  Pedido.findById(req.params.id)
+    .then((pedido, err) => {
+      pedido.info.map((id) => {
+        return mongoose.Types.ObjectId(id);
+      });
+      Producto.find({ _id: { $in: pedido.info } }).then((prods) => {
+        pedido.info = prods;
+        return res.json(pedido);
+      });
+    })
+    .catch(next);
+};
 
 const verHistorialPedidos = (req, res, next) => {
-  let filter={};
-  switch(req.usuario.type){
-    case 'client':
-      if(req.usuario.id !== req.params.id) return res.status(401).send('No autorizado');
-      filter={client:req.params.id}
+  let filter = {};
+  switch (req.usuario.type) {
+    case "client":
+      if (req.usuario.id !== req.params.id)
+        return res.status(401).send("No autorizado");
+      filter = { client: req.params.id };
       break;
-    case 'chef':
-      filter={chef:req.params.id}
+    case "chef":
+      filter = { chef: req.params.id };
       break;
-    case 'mesero':
-      filter={mesero:req.params.id}
+    case "mesero":
+      filter = { mesero: req.params.id };
       break;
-    }
-  Pedido.find(filter).then((pedido,err) => {
-    if (!pedido || err) {
-      return res.sendStatus(401)
-    }
-    return res.json(pedido);
-  }).catch(next);
-}
+  }
+  Pedido.find(filter)
+    .then((pedido, err) => {
+      if (!pedido || err) {
+        return res.sendStatus(401);
+      }
+      return res.json(pedido);
+    })
+    .catch(next);
+};
 
 const editarPedido = (req, res, next) => {
-  let filter={};
-  switch(req.usuario.type){
-    case 'client':
-      filter={_id:req.params.id,client:req.usuario.id}
+  let filter = {};
+  switch (req.usuario.type) {
+    case "client":
+      filter = { _id: req.params.id, client: req.usuario.id };
       break;
-    case 'admin':
-      filter={_id:req.params.id}
+    case "admin":
+      filter = { _id: req.params.id };
       break;
-    case 'chef':
-      filter={_id:req.params.id,chef:req.usuario.id}
+    case "chef":
+      filter = { _id: req.params.id, chef: req.usuario.id };
       break;
-    case 'mesero':
-      filter={_id:req.params.id,mesero:req.usuario.id}
+    case "mesero":
+      filter = { _id: req.params.id, mesero: req.usuario.id };
       break;
-    }
-    let datos = req.body;
-    Pedido.findOneAndUpdate(filter, { $set: datos }, { new: true }).then((pedido) => {
-    if (!pedido) {
-      return res.sendStatus(401)
-    }
-    return res.json(pedido);
-  }).catch(next);
-}
+  }
+  let datos = req.body;
+  Pedido.findOneAndUpdate(filter, { $set: datos }, { new: true })
+    .then((pedido) => {
+      if (!pedido) {
+        return res.sendStatus(401);
+      }
+      return res.json(pedido);
+    })
+    .catch(next);
+};
 
-
-
-
-function cambiarEstatusPedido(req, res, next) {//-----------listo
-  if(req.usuario.type!=="admin"){
-    return res.status(401).send("sin permisos")
+function cambiarEstatusPedido(req, res, next) {
+  //-----------listo
+  if (req.usuario.type !== "admin") {
+    return res.status(401).send("sin permisos");
   }
   //db.collection.findOneAndUpdate({busqueda},{nuevos:datos},{new:true})
-  Pedido.findOneAndUpdate({_id:req.params.id},{$set:req.body},{new:true}).then(pedido => {
-    if (!pedido) { return res.sendStatus(401); }
-    res.status(201).json(pedido)
-  }).catch(next)
+  Pedido.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: req.body },
+    { new: true }
+  )
+    .then((pedido) => {
+      if (!pedido) {
+        return res.sendStatus(401);
+      }
+      res.status(201).json(pedido);
+    })
+    .catch(next);
 }
-
 
 const filtrarPedido = (req, res, next) => {
-
-  /* 
-  fechaIni
-  fechaFin
-  idCliente
-  idPedido
-  idMesero
-  idChef
-   */
-  let campo = Object.keys(req.body)[0];//Obtiene el nombre del campo para filtrar
-  let dato = req.body[campo];//Obtiene el valor por el que se va a filtrar
-  let pedidos = PEDIDOS.filter((pedido) => {
-    let noEncontrado = true; //Se crea una variable temporal para determinar si fue encontrado algún producto
-    pedido.info_productos.forEach((producto) => { //En cada pedido, revisa los productos
-      let regex = new RegExp(dato, 'gi');//Crea una expresión regular para evaluar
-      noEncontrado = noEncontrado && !regex.test(producto[campo]);//Evalua el campo del producto a filtrar con la expresión regular. 
-      //Se hace un AND para que con la primera iteración que encuentre, no cambie el resultado en las siguientes iteraciones
-    });
-    return !noEncontrado;
-  });
-
-  if (!!pedidos[0]) {//Si no encuentra ningun pedido, regresa un error
-    res.status(200).send(pedidos);
-  } else {
-    res.status(404).send({ errorMessage: 'NotFound: Busqueda no arrojó resultados' });
+  let filter = {};
+  if (req.body.fechaIni || req.body.fechaFin) {
+    filter.createdAt = {};
+    if (req.body.fechaIni)
+      filter.createdAt["$gte"] = new Date(req.body.fechaIni);
+    if (req.body.fechaFin)
+      filter.createdAt["$lt"] = new Date(req.body.fechaFin);
   }
-}
+  if (req.body.idCliente) filter.client = mongoose.Types.ObjectId(idCliente);
+  if (req.body.idPedido) filter._id = mongoose.Types.ObjectId(idPedido);
+  if (req.body.idChef) filter.chef = mongoose.Types.ObjectId(idChef);
+  if (req.body.idMesero) filter.mesero = mongoose.Types.ObjectId(idMesero);
+
+  Pedido.find(filter).then((pedidos, err) => {
+    return res.send(pedidos);
+  });
+};
 
 const eliminarPedido = (req, res, next) => {
+  if (req.usuario.type !== "admin") {
+    return res.status(401).send("sin permisos");
+  }
+  Pedido.findOneAndDelete({ _id: req.params.id }).then((p) => {
+    //Buscando y eliminando pedido en MongoDB.
+    res.status(200).send(`Pedido ${req.params.id} eliminado: ${p}`);
+  });
+
+  /*
   let pedidoEliminado = null; //aqu'i guardara' la info del eliminado
   let encontrado = false;
 
   for (let i = 0; i < PEDIDOS.length; i++) {
-    if (PEDIDOS[i].id === +req.params.id) { //cuando encuentre el pedido acorde a su id
+    if (PEDIDOS[i].id === +req.params.id) {
+      //cuando encuentre el pedido acorde a su id
       if (!PEDIDOS[i].estatus) pedidoEliminado = PEDIDOS.splice(i, 1); //si el pedido est'a cancelado (estatus=0) se eliminara del arreglo y se guarda su info
       encontrado = true; //cambia bandera a encontrado
       break;
     }
   }
-  if (!!pedidoEliminado) { //si hay un eliminado
+  if (!!pedidoEliminado) {
+    //si hay un eliminado
     res.status(200).send(pedidoEliminado[0]); //se muestra como parte de la respuesta
-  } else if (encontrado) { //fue encontrado pero no eliminado
-    res
-      .status(409)
-      .send({
-        errorMessage: 'Conflict: No se puede eliminar un pedido no cancelado',
-      });
-  } else { //no encontrado
+  } else if (encontrado) {
+    //fue encontrado pero no eliminado
+    res.status(409).send({
+      errorMessage: "Conflict: No se puede eliminar un pedido no cancelado",
+    });
+  } else {
+    //no encontrado
     res
       .status(404)
-      .send({ errorMessage: 'Not found: No se encontró el pedido' });
-  }
-}
+      .send({ errorMessage: "Not found: No se encontró el pedido" });
+  }*/
+};
 
 // exportamos las funciones definidas
 module.exports = {
